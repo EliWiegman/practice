10d063842167854b507e834184c721b2bea59872
ebwiegman <ebwiegman@gmail.com>
2020-09-27 15:24:34 -0400

    first commit

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // access to 
+
+using namespace std;
+
+int main() {
+    int a = 5;
+    float b = 0.9;
+    float c = a + b;
+    // cout << a + b + "\n";
+    cout << c << "\n";
+}
---
+++ helloworld.cpp
@@ -0,0 +1,5 @@
+#include <iostream> // access to 
+
+int main() {
+    std::cout << "hello world!\n";
+}
